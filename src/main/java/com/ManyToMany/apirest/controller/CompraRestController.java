24c075f9fcc98b978bc06d2fe071e4319da6e3d4
Kevin Fdez <kevinfv2000@hotmail.com>
2022-03-02 15:31:56 +0100

    Commit CompraController

--- src/main/java/com/ManyToMany/apirest/controller/CompraRestController.java
+++ src/main/java/com/ManyToMany/apirest/controller/CompraRestController.java
@@ -39,7 +39,6 @@ public class CompraRestController {
 		try {
 			compra = servicio.findById(id);
 		} catch (DataAccessException e) {
-			// TODO: handle exception
 			response.put("mensaje", "Error al realizar la consulta.");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -60,7 +59,6 @@ public class CompraRestController {
 		try {
 			compraNew = servicio.save(compra);
 		} catch (DataAccessException e) {
-			// TODO: handle exception
 			response.put("mensaje", "Error al realizar la consulta.");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -88,7 +86,6 @@ public class CompraRestController {
 			servicio.save(compraActual);
 
 		} catch (DataAccessException e) {
-			// TODO: handle exception
 			response.put("mensaje", "Error al realizar la consulta.");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
